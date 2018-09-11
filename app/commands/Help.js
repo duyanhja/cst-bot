db332c449c5434524e75e733f4e5a2f933cb0487
James <duyanh.j@gmail.com>
2018-09-10 23:31:58 -0700

    updated help function to account for delete command

--- app/commands/Help.js
+++ app/commands/Help.js
@@ -5,6 +5,7 @@ let helpMessage = (author) => {
   help += "Looks like you're confused. Here's what I've learned: \n\n";
   help += "**todo**  |  Displays the todo list\n";
   help += "**add** *<month>* *<day>* *<time>* *<descr>*  |  add an event to the todo list (MM DD HH:mm Birthday)\n";
+  help += "**del** *<index>* | deletes todo item based on its index on the list\n"
   help += "**prefix** *<prefix>*  |  change the bot's prefix (default: &)"
 
   return help;
